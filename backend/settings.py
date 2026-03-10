1e41fefb84281f759650692216aa4bfbd7980772
Leandro Gomez <gelivanl.gomez212@umariana.edu.co>
2026-03-10 10:45:45 -0500

    Fix: Agregar middlewares faltantes para admin

--- backend/settings.py
+++ backend/settings.py
@@ -171,15 +171,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'monitoreo.consentimiento_middleware.ConsentimientoMiddleware', #
-    #'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
